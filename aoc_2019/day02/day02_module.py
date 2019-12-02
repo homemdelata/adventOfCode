15fa33e154fa6724a6e72843b11e7eede6b1d0fd
Gustavo Ayres <tatoayres@gmail.com>
2019-12-02 09:03:17 -0300

    novos testes do programa

--- aoc_2019/day02/day02_module.py
+++ aoc_2019/day02/day02_module.py
@@ -2,10 +2,13 @@ class UserQuit( Exception ): pass
 
 def intCode(opCode, value1, value2):
     if opCode == 1:
-        return 0
+        return value1 + value2
     elif opCode == 2:
-        return 0
+        return value1 * value2
     elif opCode == 99:
         raise UserQuit
     else:
         raise ValueError
+
+def program(arrayInput):
+    pass
--- aoc_2019/day02/test_day02.py
+++ aoc_2019/day02/test_day02.py
@@ -2,6 +2,7 @@ import pytest
 
 from day02_module import intCode
 from day02_module import UserQuit
+from day02_module import program
 
 def test_day02_opcode1():
     opCode = 1
@@ -26,9 +27,33 @@ def test_day02_opcode99():
 
 
 def test_day02_opcode_wrong():
-    array = [3,2,3,3]
     with pytest.raises(ValueError):
         opCode = 30
         value1 = 2
         value2 = 3
         result = intCode(opCode, value1, value2)
+
+def test_day02_testProgram01():
+    arrayInput = [1,9,10,3,2,3,11,0,99,30,40,50]
+    program(arrayInput)
+    assert arrayInput == [3500,9,10,70,2,3,11,0,99,30,40,50]
+
+def test_day02_testProgram02():
+    arrayInput = [1,0,0,0,99]
+    program(arrayInput)
+    assert arrayInput == [2,0,0,0,99]
+
+def test_day02_testProgram03():
+    arrayInput = [2,3,0,3,99]
+    program(arrayInput)
+    assert arrayInput == [2,3,0,6,99]
+
+def test_day02_testProgram04():
+    arrayInput = [2,4,4,5,99,0]
+    program(arrayInput)
+    assert arrayInput == [2,4,4,5,99,9801]
+
+def test_day02_testProgram05():
+    arrayInput = [1,1,1,4,99,5,6,0,99]
+    program(arrayInput)
+    assert arrayInput == [30,1,1,4,2,5,6,0,99]
