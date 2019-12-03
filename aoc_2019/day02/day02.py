53c9645fded9f62e2c02f93aa39a010e155b2a48
Gustavo Ayres <tatoayres@gmail.com>
2019-12-02 22:15:24 -0300

    PARTE 2 (acoxambrada por preguiça)

--- aoc_2019/day02/day02.py
+++ aoc_2019/day02/day02.py
@@ -11,3 +11,17 @@ with open(os.path.join(sys.path[0], 'day02_input.txt'), 'r') as inputFile:
     program(arrayInput)
 
     print(arrayInput[0])
+
+    expectedResult = 19690720
+    noun = 0
+    verb = 0
+    for noun in range(99):
+        for verb in range(99):
+            arrayInput = list(map(int, lines[0].split(',')))
+            arrayInput[1] = noun
+            arrayInput[2] = verb
+            program(arrayInput)
+            if arrayInput[0] == expectedResult:
+                print('noun: {}'.format(noun))
+                print('verb: {}'.format(verb))
+                print('result: {}'.format(100 * noun + verb))
