e301b5467a8741886ed38d19c5e596a303922bbf
Gustavo Ayres <gustavo.ayres@zbra.dev>
2022-12-03 09:34:37 -0300

    a few reactors to make some code more readable

--- 2022/Day01/day01.py
+++ 2022/Day01/day01.py
@@ -7,10 +7,11 @@ def parse_elves(input):
     str_elves = [elf.split("\n") for elf in input.strip().split("\n\n")]
     return [list(map(int, i)) for i in str_elves]
 
+
 def calculate_total_calories(elf):
     return sum(elf)
-    
-    
+
+
 def day01_part1(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
         input_test = file.read()
@@ -18,6 +19,7 @@ def day01_part1(input_name):
         elves_calories = list(map(calculate_total_calories, elves))
         highest_calories = max(elves_calories)
     print("Day 01 - Part 1 - {}: {}".format(input_name, highest_calories))
+
 
 def day01_part2(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
@@ -29,9 +31,9 @@ def day01_part2(input_name):
     print("Day 01 - Part 2 - {}: {}".format(input_name, top3_calories))
 
 
+if __name__ == "__main__":
+    day01_part1('test.txt')
+    day01_part1('input.txt')
 
-day01_part1('test.txt')
-day01_part1('input.txt')
-
-day01_part2('test.txt')
-day01_part2('input.txt')
+    day01_part2('test.txt')
+    day01_part2('input.txt')
--- 2022/Day02/day02.py
+++ 2022/Day02/day02.py
@@ -2,6 +2,7 @@ from enum import Enum
 import os
 import sys
 
+
 class jokenpo_hand(Enum):
     rock = 1
     paper = 2
@@ -25,6 +26,7 @@ def opponent_hand_converter(opponent_hand):
         case _:
             raise Exception("Hand not mapped")
 
+
 def your_hand_converter(your_hand):
     match your_hand:
         case "X":
@@ -36,6 +38,7 @@ def your_hand_converter(your_hand):
         case _:
             raise Exception("Hand not mapped")
 
+
 def expected_end_converter(expected_end):
     match expected_end:
         case "X":
@@ -46,7 +49,21 @@ def expected_end_converter(expected_end):
             return jokenpo_result.win
         case _:
             raise Exception("End not mapped")
-    
+
+
+def get_winning_hand(opponent_hand):
+    if opponent_hand == jokenpo_hand.scissors:
+        return jokenpo_hand.rock
+    else:
+        return jokenpo_hand(opponent_hand.value + 1)
+
+
+def get_losing_hand(opponent_hand):
+    if opponent_hand == jokenpo_hand.rock:
+        return jokenpo_hand.scissors
+    else:
+        return jokenpo_hand(opponent_hand.value - 1)
+
 
 def jokenpo_score(opponent, you):
     opponent_hand = opponent_hand_converter(opponent)
@@ -58,32 +75,29 @@ def jokenpo_score(opponent, you):
         result = jokenpo_result.win
     return result.value + your_hand.value
 
+
+def total_score(strategy):
+    return sum([jokenpo_score(i[0], i[1]) for i in strategy])
+
+
 def correct_jokenpo_score(opponent, expected_end):
     opponent_hand = opponent_hand_converter(opponent)
     expected_end_result = expected_end_converter(expected_end)
     match expected_end_result:
         case jokenpo_result.lost:
-            if opponent_hand == jokenpo_hand.rock:
-                your_hand = jokenpo_hand.scissors
-            else:
-                your_hand = jokenpo_hand(opponent_hand.value - 1)
+            your_hand = get_losing_hand(opponent_hand)
         case jokenpo_result.draw:
             your_hand = opponent_hand
         case jokenpo_result.win:
-            if opponent_hand == jokenpo_hand.scissors:
-                your_hand = jokenpo_hand.rock
-            else:
-                your_hand = jokenpo_hand(opponent_hand.value + 1)
+            your_hand = get_winning_hand(opponent_hand)
         case _:
             raise Exception("Result not mapped")
     return expected_end_result.value + your_hand.value
-            
 
-def total_score(strategy):
-    return sum([jokenpo_score(i[0], i[1]) for i in strategy])
 
 def correct_total_score(strategy):
     return sum([correct_jokenpo_score(i[0], i[1]) for i in strategy])
+
 
 def day02_part1(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
@@ -92,6 +106,7 @@ def day02_part1(input_name):
         score = total_score(strategy)
     print("Day 02 - Part 1 - {}: {}".format(input_name, score))
 
+
 def day02_part2(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
         input = file.read()
@@ -99,9 +114,10 @@ def day02_part2(input_name):
         score = correct_total_score(strategy)
     print("Day 02 - Part 2 - {}: {}".format(input_name, score))
 
-    
-day02_part1("test.txt")
-day02_part1("input.txt")
 
-day02_part2("test.txt")
-day02_part2("input.txt")
+if __name__ == "__main__":
+    day02_part1("test.txt")
+    day02_part1("input.txt")
+
+    day02_part2("test.txt")
+    day02_part2("input.txt")
