b317edee0ae0c41b65da4d39644c958ea0f1a006
Gustavo Ayres <gustavo.ayres@zbra.dev>
2022-12-03 11:12:54 -0300

    Day 3 - part 1 solution

--- 2022/Day03/day03.py
+++ 2022/Day03/day03.py
@@ -1,30 +1,36 @@
 import os
+import string
 import sys
 
+def parse_rucksacks(input):
+    return input.strip().split("\n")
 
 def get_duplicate_item(rucksack):
-    pass
+    half_rucksack = int(len(rucksack)/2)
+    first_compartiment = rucksack[:half_rucksack]
+    second_compartiment = rucksack[-half_rucksack:]
+    return list(set(first_compartiment).intersection(second_compartiment))[0]
 
 
 def get_item_priority(item):
-    pass
+    priority_list = string.ascii_lowercase[:26] + string.ascii_uppercase[:26]
+    return priority_list.index(item) + 1
 
 
 def sum_priorities(rucksack_list):
-    pass
+    return sum([get_item_priority(get_duplicate_item(rucksack)) for rucksack in rucksack_list])
 
 
 def day03_part1(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
-        result = None
-        pass
+        input = file.read()
+        result = sum_priorities(parse_rucksacks(input))
     print("Day 03 - Part 1 - {}: {}".format(input_name, result))
 
 
 def day03_part2(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
         result = None
-        pass
     print("Day 03 - Part 2 - {}: {}".format(input_name, result))
 
 
--- 2022/Day03/test_day03.py
+++ 2022/Day03/test_day03.py
@@ -17,24 +17,25 @@ def test_given_rucksack_then_return_duplicate_item(rucksack, duplicate_item):
 
 @pytest.mark.parametrize("item, expected_priority",
                          [
-                            ("p", 16),
-                            ("L", 38),
-                            ("P", 42),
-                            ("v", 22),
-                            ("t", 20),
-                            ("s", 19)
+                             ("p", 16),
+                             ("L", 38),
+                             ("P", 42),
+                             ("v", 22),
+                             ("t", 20),
+                             ("s", 19)
                          ])
 def test_given_strategy_then_result(item, expected_priority):
     assert get_item_priority(item) == expected_priority
 
+
 @pytest.mark.parametrize("rucksack_list, expected_total",
                          [(
-                             ["vJrwpWtwJgWrhcsFMMfFFhFp"
-"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-"PmmdzqPrVvPwwTWBwg",
-"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-"ttgJtRGJQctTZtZT",
-"CrZsJsPPZsGzwwsLwLmpwMDw"], 157
+                             ["vJrwpWtwJgWrhcsFMMfFFhFp",
+                              "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                              "PmmdzqPrVvPwwTWBwg",
+                              "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                              "ttgJtRGJQctTZtZT",
+                              "CrZsJsPPZsGzwwsLwLmpwMDw"], 157
                          )])
 def test_given_rucksack_list_then_get_total_priorities(rucksack_list, expected_total):
     assert sum_priorities(rucksack_list) == expected_total
