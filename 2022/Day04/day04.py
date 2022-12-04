9311a6963822b0f45caadc5b0abffcab938dd1fb
Gustavo Ayres <gustavo.ayres@zbra.dev>
2022-12-04 16:39:51 -0300

    Day 4 - Part 1 solution

--- 2022/Day04/day04.py
+++ 2022/Day04/day04.py
@@ -2,16 +2,25 @@ import os
 import string
 import sys
 
-def fully_contains(fisrt_section, second_section):
-    pass
+def parse_pairs_list(input):
+    return [[list(map(int, section.split("-"))) for section in pair.split(",")] for pair in input.strip().split("\n")]
+
+def fully_contains(pair):
+    first_section = pair[0]
+    second_section = pair[1]
+    if first_section[0] <= second_section[0] and first_section[1] >= second_section[1]:
+        return True
+    elif second_section[0] <= first_section[0] and second_section[1] >= first_section[1]:
+        return True
+    return False
 
 def count_fully_contained(groups):
-    pass
+    return list(map(fully_contains, groups)).count(True)
 
 def day04_part1(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
         input = file.read()
-        result = None
+        result = count_fully_contained(parse_pairs_list(input))
     print("Day 04 - Part 1 - {}: {}".format(input_name, result))
 
 
--- 2022/Day04/test_day04.py
+++ 2022/Day04/test_day04.py
@@ -12,7 +12,7 @@ from day04 import fully_contains, count_fully_contained
                              ([[2,6], [4,8]], False)
                          ])
 def test_given_two_sections_groups_then_check_if_fully_contains(sections_pair, expected_fully_contains):
-    assert fully_contains(sections_pair[0], sections_pair[1]) == expected_fully_contains
+    assert fully_contains(sections_pair) == expected_fully_contains
 
 
 @pytest.mark.parametrize("pairs_list, expected_count",
