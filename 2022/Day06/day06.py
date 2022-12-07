d2ebeac47243ca63f566afc51d2a31aeda827446
Gustavo Ayres <gustavo.ayres@zbra.dev>
2022-12-06 22:32:10 -0300

    all solutions

--- 2022/Day06/day06.py
+++ 2022/Day06/day06.py
@@ -1,21 +1,31 @@
+from collections import Counter
 import os
 import sys
 from datetime import datetime
 
+
 def get_starter_marker(message):
-    pass
+    bla = [message[i:i+4] for i in range(len(message))]
+    ble = [i for i in range(len(bla)) if len(set(bla[i])) == 4]
+    return ble[0] + 4
+
+def get_starter_message(message):
+    bla = [message[i:i+14] for i in range(len(message))]
+    ble = [i for i in range(len(bla)) if len(set(bla[i])) == 14]
+    return ble[0] + 14
+
 
 def day06_part1(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
         input = file.read()
-        result = None
+        result = get_starter_marker(input)
     print("Day 06 - Part 1 - {}: {}".format(input_name, result))
 
 
 def day06_part2(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
         input = file.read()
-        result = None
+        result = get_starter_message(input)
     print("Day 06 - Part 2 - {}: {}".format(input_name, result))
 
 
