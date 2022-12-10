49d55e335111ab1ff4e5b5aa1b2e3117a5e8060d
Gustavo Ayres <gustavo.ayres@zbra.dev>
2022-12-10 17:39:17 -0300

    Day 8 part 2 test red

--- 2022/Day08/test_day08.py
+++ 2022/Day08/test_day08.py
@@ -1,5 +1,5 @@
 import pytest
-from day08 import count_visible_trees
+from day08 import count_visible_trees, higher_viewing_distance
 
 
 @pytest.mark.parametrize("trees_map, expected_visible_count",
@@ -14,3 +14,16 @@ from day08 import count_visible_trees
                         ])
 def test_given_trees_map_then_count_visible(trees_map, expected_visible_count):
     assert count_visible_trees(trees_map) == expected_visible_count
+
+@pytest.mark.parametrize("trees_map, expected_higher_viewing_distance",
+                        [
+("""
+30373
+25512
+65332
+33549
+35390
+""", 8)
+                        ])
+def test_given_trees_map_then_higher_viewing_distance(trees_map, expected_higher_viewing_distance):
+    assert higher_viewing_distance(trees_map) == expected_higher_viewing_distance
