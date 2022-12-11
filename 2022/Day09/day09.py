d30f73da72a6a527817eb314c3f40b358e0c07df
Gustavo Ayres <gustavo.ayres@zbra.dev>
2022-12-11 08:06:39 -0300

    day 9 part 2 test red

--- 2022/Day09/day09.py
+++ 2022/Day09/day09.py
@@ -44,6 +44,45 @@ def tail_postitions(motions_input):
     return len(list_tail_positions)
 
 
+def longer_rope_tail_postitions(motions_input):
+    motions = parse_motions(motions_input)
+    current_head_position = (0, 0)
+    current_tail_position = (0, 0)
+    list_tail_positions = [current_tail_position]
+    for motion in motions:
+        head_direction = (0, 0)
+        match motion[0]:
+            case "U":
+                head_direction = (0, 1)
+            case "R":
+                head_direction = (1, 0)
+            case "D":
+                head_direction = (0, -1)
+            case "L":
+                head_direction = (-1, 0)
+            case _:
+                raise Exception("Motion not mapped")
+        
+        for i in range(motion[1]):
+            current_head_position = tuple(map(operator.add, current_head_position, head_direction))
+            delta = tuple(map(operator.sub, current_head_position, current_tail_position))
+            if abs(delta[0]) > 1 or abs(delta[1]) > 1:
+                x = 0
+                y = 0
+                if abs(delta[0]) != 0:
+                    x = int(delta[0]/abs(delta[0]))
+                if abs(delta[1]) != 0:
+                    y = int(delta[1]/abs(delta[1]))
+                tail_direction = (x, y)
+                current_tail_position = tuple(map(operator.add, current_tail_position, tail_direction))
+            
+            if current_tail_position not in list_tail_positions:
+                list_tail_positions.append(current_tail_position)
+    
+    return len(list_tail_positions)
+
+
+
 def day09_part1(input_name):
     with open(os.path.join(sys.path[0], input_name), 'r') as file:
         input = file.read()
--- 2022/Day09/test_day09.py
+++ 2022/Day09/test_day09.py
@@ -1,5 +1,5 @@
 import pytest
-from day09 import tail_postitions
+from day09 import tail_postitions, longer_rope_tail_postitions
 
 @pytest.mark.parametrize("motions, expected_tail_positions",
                         [
@@ -18,3 +18,28 @@ def test_motions_then_tail_positions(motions, expected_tail_positions):
     assert tail_postitions(motions) == expected_tail_positions
 
 
+@pytest.mark.parametrize("motions, expected_tail_positions",
+                        [
+("""
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+""", 1),
+("""
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+""", 36)
+                        ])
+def test_motions_then_tail_positions_longer_rope(motions, expected_tail_positions):
+    assert longer_rope_tail_postitions(motions) == expected_tail_positions
